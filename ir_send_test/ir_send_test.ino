232df1ba2100013e5b232f26420ab7db46777bf4
Joshua Byers <legomanmc@gmail.com>
2023-04-13 01:36:35 -0400

    Added instructions on how to send hex data through IR

--- ir_send_test/ir_send_test.ino
+++ ir_send_test/ir_send_test.ino
@@ -1,13 +1,30 @@
+#include <IRLibAll.h>
+
+IRsend mySender;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(1, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  mySender.
 }
 
+/* Instructions on how to set data that you want to send
+ *    when using IRLibAll IRsend.send() with IRrecv from
+ *    IRremote.hpp:
+ * Example data: 0xDEADBEEF
+ * -First, you need to reverse the order of the hex digits
+ *  So, 0xDEADBEEF -> 0xFEEBDAED
+ * -Then, for each of the digits, you reverse their bits.
+ *  So, 0xFEEBDAED -> 0xF77DB57B
+ * Now when you send 0xF77DB57B, the receiver will read
+ *    0xDEADBEEF.
+*/
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("Hello, fellow Arduino!");
-  delay(1000);
+  if (Serial.read() != -1) {
+    //send a code every time a character is received from the serial port
+    //Sony DVD power A8BCA
+    mySender.send(NEC, 0xF77DB57B, 32);
+    Serial.println("Sent message");
+    delay(40);
+  }
 }
